365d4ebb2fc9bf19f4131cdd2d8d3372cddaaffa
sanjaykumargouda <34412297+sanjaykumargouda@users.noreply.github.com>
2017-12-21 09:02:31 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { DynamicFormComponent } from './dynamic-form/dynamic-form/dynamic-form.component';
 
 @Component({
@@ -6,19 +7,36 @@ import { DynamicFormComponent } from './dynamic-form/dynamic-form/dynamic-form.c
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  // testForm: FormGroup;
   config: any[] = [
     {
       type: 'input',
       label: 'Full name',
       name: 'name',
+      required: 'true',
       placeholder: 'Enter your name'
+    },
+    {
+      type: 'input',
+      label: 'Last name',
+      name: 'lname',
+      required: 'false',
+      placeholder: 'Enter your last name'
+    },
+    {
+      type: 'input',
+      label: 'Phone Number',
+      name: 'cell',
+      required: 'false',
+      placeholder: 'Enter your phone number'
     },
     {
       type: 'select',
       label: 'Favourite food',
       name: 'food',
+      required: 'true',
       options: ['Pizza', 'Hot Dogs', 'Knakworstje', 'Coffee'],
       placeholder: 'Select an option'
     },
@@ -28,11 +46,19 @@ export class AppComponent {
       type: 'button'
     }
   ]
-  constructor() {
-    
+  constructor(/* private fb: FormBuilder */) {
   }
+  ngOnInit() {
+   /* this.testForm = this.fb.group({
+    user: new FormControl(null, Validators.required),
+    pass: new FormControl(null, Validators.required)
+   }) */
+  }
+  /* testFormSubmit(val) {
+    console.log(val, 'test');
+  }*/
+  
   formSubmitted(e) {
     console.log(e);
-
   }
 }
